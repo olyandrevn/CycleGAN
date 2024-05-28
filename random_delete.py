20653fb30ee080d0bee9c1067d368fec4c8c3385
yule44ka <julia.lutova.2004@mail.ru>
2024-05-28 18:01:55 +0300

    test-train created

--- random_delete.py
+++ random_delete.py
@@ -3,22 +3,17 @@ import random
 
 
 def delete_random_files(directory, number_of_files_to_delete=0):
-    # Получаем список всех файлов в указанной директории
     all_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
-    # Проверяем, что в директории достаточно файлов для удаления
     if len(all_files) < number_of_files_to_delete:
         print(f"Недостаточно файлов для удаления. Всего файлов: {len(all_files)}")
         return
 
-    # Случайным образом выбираем файлы для удаления
     files_to_delete = random.sample(all_files, number_of_files_to_delete)
 
-    # Удаляем выбранные файлы
     for file in files_to_delete:
         os.remove(os.path.join(directory, file))
         print(f"Удалён файл: {file}")
 
 
-# Пример использования
 delete_random_files("gzhel", 659)
--- squaring.py
+++ squaring.py
@@ -3,26 +3,20 @@ import os
 
 
 def make_images_square(directory):
-    # Получаем список всех файлов в указанной директории
     for filename in os.listdir(directory):
         if filename.endswith(".jpg"):
             filepath = os.path.join(directory, filename)
             image = Image.open(filepath)
             width, height = image.size
 
-            # Вычисляем размер стороны квадрата
             new_size = max(width, height)
 
-            # Создаем новый квадратный белый фон
             new_image = Image.new("RGB", (new_size, new_size), (255, 255, 255))
 
-            # Вставляем исходное изображение на новый фон
             new_image.paste(image, ((new_size - width) // 2, (new_size - height) // 2))
 
-            # Сохраняем новое изображение в ту же директорию
             new_image.save(filepath)
 
 
-# Пример использования
 make_images_square("khokhloma")
 make_images_square("gzhel")
